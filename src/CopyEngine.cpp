0aebda6de3585f23d3524c4738563517442bd014
bootsareme <73851560+bootsareme@users.noreply.github.com>
2026-04-17 20:57:02 -0400

    implement destructor cleanup

--- src/CopyEngine.cpp
+++ src/CopyEngine.cpp
@@ -6,4 +6,23 @@ CopyEngine::CopyEngine(Arguments args)
 
 CopyEngine::~CopyEngine()
 {
+	if (inputFile != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(inputFile);
+		inputFile = INVALID_HANDLE_VALUE;
+	}
+
+	if (outputFile != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(outputFile);
+		outputFile = INVALID_HANDLE_VALUE;
+	}
+
+	if (this->buffer)
+	{
+		delete[] this->buffer;
+		this->buffer = nullptr;
+	}
+
+	bufSize = 0;
 }
